49bd4a8e36997e1d34a3031747ae84df5040f1e3
DwayneElliott <123275932+DwayneElliott@users.noreply.github.com>
2023-01-21 18:25:01 -0600

    Update Letter Count.py

--- Letter Count.py
+++ Letter Count.py
@@ -1,22 +1,22 @@
 def Letter_Count():
-    prompt='Please enter a word.\n'
+    prompt='Please enter a word:\n'
     word=input(prompt)
     count=0
-    prompt1='Please enter letter to count\n'
-    letters=input(prompt1)
+    prompt='Please enter a letter to count:\n'
+    letters=input(prompt)
     for letter in word:
         if letter==letters:
             count=count+1
-    print("Number of letters's in",word,':',count)
-    prompt2='Would you like to exit?\n'
-    restart=input(prompt2)
+    print("Number of",letters,"'s in",word,':',count)
+    prompt='Would you like to exit?\n'
+    restart=input(prompt)
     if (restart=='Yes' or restart=='yes'):
         exit
     if (restart=='No' or restart=='no'):
         Letter_Count()
     else:
         exit
-Letter_Count()    
+Letter_Count()       
 
     
 
